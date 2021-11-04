e684d2c24193242eda46fcf1f3a590f597021ac2
DataJackOH <74881540+DataJackOH@users.noreply.github.com>
2021-11-04 20:24:50 +1100

    mga data change

--- mba_data.py
+++ mba_data.py
@@ -8,13 +8,54 @@ from mlxtend.frequent_patterns import apriori
 from mlxtend.frequent_patterns import association_rules
 # Import dependencies
 import streamlit as st
-import streamlit.components.v1 as components
+import streamlit.components as components
 import pandas as pd
 import networkx as nx
 from pyvis.network import Network
 import pathlib
 
-uploaded_file = st.file_uploader('Choose a file', type=['csv','xlsx'])
+
+# An alias for our state
+state = st.session_state
+
+
+# A function to easily go from one step to another
+def change_step(next_step):
+    state.step = next_step
+
+
+# Let's initialize our session state
+if "data" not in state:
+    state.data = []
+    state.step = "init"
+
+# Page config
+st.set_page_config(page_title="Market Basket Analysis", layout="wide")
+
+
+# Step 1
+if state.step == "init":
+    st.button("Load data", on_click=change_step, args=["load"])
+
+
+uploaded_file = st.sidebar.file_uploader('Upload a file with some order data', type=['csv','xlsx'])
+
+demo = st.sidebar.checkbox('Use demo data')
+
+
+
+
+## add in context
+
+
+# Info
+with st.beta_expander("What is market basket analysis?", expanded=False):
+    st.write('Market basket analysis is useful for finding hidden associations between orders.')
+    st.write("The key metrics are lift and confidence. Lift is a measure of")
+
+
+    
+
 
 if uploaded_file is not None:
 #read csv
@@ -22,32 +63,64 @@ if uploaded_file is not None:
         df =pd.read_csv(uploaded_file)
     except:
         df =pd.read_excel(uploaded_file)
+elif demo:
+    df = pd.read_csv('https://raw.githubusercontent.com/DataJackOH/marketbasket/main/orderdataset.csv')
 else:
-    st.warning('you need to upload a csv or excel file.')
+    st.title('Market Basket Analysis')
+    st.header("""First, upload some data via the sidebar.""" )
+    st.header("""This should be a csv or xslx file with one row per order.""")
+    st.header("""The only fields you need are 
+    1. Item Description
+    2. Quantity
+    3. A common Order/InvoiceID""")
+    st.stop()
+    
 
 # In[2]:
 
 st.title('Market Basket Analysis')
+st.header("Nice one! Now lets map your data to the columns we're expecting")
+
+st.subheader("""
 
 
 
-def clean_up(df, description='Description', orders='InvoiceNo'):
+""")
+
+col1, col2, col3 = st.beta_columns(3)
+with st.form(key='columns_in_form'):
+        product_description = col1.selectbox('The column which contains your product description', df.columns)
+        order_number = col2.selectbox('The column which contains your order/invoice number.', df.columns)
+        quantity = col3.selectbox('The column which contains the quantity of the product within that order', df.columns)
+        #orderdate = col1.selectbox('The column which contains the date of the order', df.columns)
+        submit_button = st.form_submit_button(label='Run the numbers!')
+
+
+if not submit_button:
+    st.stop()
+st.success('Boom!')
+
+@st.cache
+def clean_up(df, description=product_description, orders=order_number):
     df[description] = df[description].str.strip()
     df.dropna(axis=0, subset=[orders], inplace=True)
     df[orders] = df[orders].astype('str')
     return df
 
 
+
 # In[5]:
 
 
-clean_up(df=df)
+df = clean_up(df=df)
 
+ 
 
 # In[6]:
 
 
-def rearrange_df(df=df, description='Description', orders='InvoiceNo', quantity='Quantity'):
+@st.cache
+def rearrange_df(df=df, description=product_description, orders=order_number, quantity=quantity):
     basket = (df
           .groupby([orders, description])[quantity]
           .sum().unstack().reset_index().fillna(0)
@@ -75,7 +148,6 @@ def encode_units(x):
         return 1
 
 basket_sets = basket.applymap(encode_units)
-basket_sets.drop('POSTAGE', inplace=True, axis=1)
 basket= None
 
 
@@ -87,21 +159,32 @@ basket_sets = None
 
 
 # In[11]:
-
-
-rules = association_rules(frequent_itemsets, metric="lift", min_threshold=1)
-rules["antecedents"] = rules["antecedents"].apply(lambda x: ', '.join(list(x))).astype("unicode")
-rules["consequents"] = rules["consequents"].apply(lambda x: ', '.join(list(x))).astype("unicode")
-rules = rules[['antecedents','consequents','support','lift']]
-frequent_itemsets = None
+@st.cache
+def associationcleanup(frequent_itemsets=frequent_itemsets):
+    rules = association_rules(frequent_itemsets, metric="lift", min_threshold=1)
+    rules["product_a"] = rules["antecedents"].apply(lambda x: ', '.join(list(x))).astype("unicode")
+    rules["product_b"] = rules["consequents"].apply(lambda x: ', '.join(list(x))).astype("unicode")
+    rules["antecedent_len"] = rules["antecedents"].apply(lambda x: len(x))
+    rules = rules[['product_a','product_b','support','confidence','lift','antecedent_len']]
+    frequent_itemsets = None
+    return rules 
+    
+rules = associationcleanup(frequent_itemsets)
 
 
 # In[12]:
-
-st.write(rules)
-
-# In[ ]:
+st.dataframe(rules)
 
 
+@st.cache
+def convert_df(df):
+     # IMPORTANT: Cache the conversion to prevent computation on every rerun
+     return df.to_csv().encode('utf-8')
 
+csvconvert = convert_df(rules)
 
+st.download_button(
+    label="Download data as CSV",
+    data=csvconvert,
+    file_name='marketbasketanalysis.csv',
+    mime='text/csv')
